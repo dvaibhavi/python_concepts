6ba6d780f46008c65edb2939548b78e379d361a0
Vaibhavi_Deshpande <vaibhavi_deshpande@epam.com>
2023-05-01 19:47:43 +0200

    added thread safe and serializable code for singleton dp

--- numpy_code.py
+++ numpy_code.py
@@ -18,7 +18,25 @@ print(df)
 filtered_data = df[df['sport']=='a']
 print(filtered_data, '\n', df[df['players']=='a'])
 
+# Example of performing data analysis using Python libraries like NumPy and Pandas
+# Create a NumPy array with some data
+data1 = np.array([[1,2,3], [4,5,6], [7,8,9]])
 
+# Create a Pandas DataFrame from the NumPy array
+df1 = pd.DataFrame(data1, columns= [ "A", "B", "C"])
+
+# Calculate the mean of the values in column A
+mean1 = df1.mean()
+
+print("mean is", mean1, "\n") 
+''' 
+output :
+mean is A    4.0
+B    5.0
+C    6.0
+dtype: float64 
+
+'''
 import plotly.express as px
 
 x = np.array([1,2,3,4,5])
@@ -32,4 +50,6 @@ y1 = np.array([15, 0, 9, -2])
 
 fig1 = px.bar(x=x1, y=y1)
 fig1.show()
+
+
 
--- singleton.py
+++ singleton.py
@@ -19,3 +19,64 @@ s1 = Singleton.get_instance()
 s2 = Singleton.get_instance()
 
 print(s1 is s2) # Output: True
+
+'''
+When implementing a singleton in Python, it's important to consider thread-safety 
+and serializability to ensure that the singleton object is always consistent and 
+can be safely accessed from multiple threads or processes. Here are two approaches to 
+implement a thread-safe and serializable singleton in Python:
+
+1. Using a metaclass
+A metaclass is a class that defines the behavior of other classes. 
+By using a metaclass, you can ensure that only one instance of a class is created and 
+that it is thread-safe.
+'''
+import threading
+
+class SingletonMeta(type):
+    _instances = {}
+    _lock = threading.Lock()
+
+    def __call__(cls, *args, **kwargs):
+        with cls._lock:
+            if cls not in cls._instances:
+                instance = super().__call__(*args, **kwargs)
+                cls._instances[cls] = instance
+        return cls._instances[cls]
+
+class MyClass(metaclass=SingletonMeta):
+    pass
+
+'''
+
+In this example, we define a SingletonMeta metaclass that keeps track of the 
+instances of the classes that use it. We use a lock to ensure that the instance creation 
+is thread-safe. The __call__ method of the metaclass is called whenever an instance of a class 
+that uses the metaclass is created. It checks whether an instance of the class already exists and,
+ if not, creates a new instance and stores it in the _instances dictionary.
+
+
+2. Using a decorator
+A decorator is a function that takes a function or a class and returns a new function or
+ class with additional behavior. By using a decorator, you can wrap a class in a function that 
+ ensures that only one instance of the class is created and that it is thread-safe.
+'''
+
+import threading
+
+def singleton(cls):
+    _instances = {}
+    _lock = threading.Lock()
+
+    def get_instance(*args, **kwargs):
+        with _lock:
+            if cls not in _instances:
+                instance = cls(*args, **kwargs)
+                _instances[cls] = instance
+        return _instances[cls]
+
+    return get_instance
+
+@singleton
+class MyClass:
+    pass
