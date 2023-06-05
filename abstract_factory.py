13355e0aae1cff1db07578cd147d8be368ec9413
Vaibhavi_Deshpande <vaibhavi_deshpande@epam.com>
2023-06-05 10:51:23 +0200

    added file excercises

--- abstract_factory.py
+++ abstract_factory.py
@@ -4,7 +4,13 @@ You would need to implement it separately to read the application configuration 
 
 source :- https://refactoring.guru/design-patterns/abstract-factory 
 '''
-
+# / The abstract factory interface declares a set of methods that
+# // return different abstract products. These products are called
+# // a family and are related by a high-level theme or concept.
+# // Products of one family are usually able to collaborate among
+# // themselves. A family of products may have several variants,
+# // but the products of one variant are incompatible with the
+# // products of another variant.
 from abc import ABC, abstractmethod
 
 class Button(ABC):
